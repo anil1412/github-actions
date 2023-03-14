ec5834200dbc67a811f5e80cf6125b49956fea1c
anil1412 <52968908+anil1412@users.noreply.github.com>
2023-03-14 17:16:52 +0530

    Update details.py

--- scripts/details.py
+++ scripts/details.py
@@ -1,15 +1,8 @@
 import json
 import sys
 
-#str = sys.argv[1]
-#print('###' in str)
-
 str = ' '.join(sys.argv)
-#print(str)
-#print(len(sys.argv)-1)
-
 str_i = str.split("###")
-#print(str_i[1])
 customer_name = str_i[1].split("---")[2].strip()
 initial_admin = str_i[2].split("---")[2].strip()
 print(f"{customer_name}|{initial_admin}")
