e334a67443f987074b171ac585e5c33763e5ee8c
anil1412 <52968908+anil1412@users.noreply.github.com>
2023-03-14 16:25:24 +0530

    Update details.py

--- scripts/details.py
+++ scripts/details.py
@@ -3,7 +3,9 @@ import sys
 
 str = sys.argv[1]
 
-str_i = str.split("###")
-customer_name = str_i[1].split("---")[2].strip()
-initial_admin = str_i[2].split("---")[2].strip()
-print(f"{customer_name}|{initial_admin}")
+print(str)
+
+#str_i = str.split("###")
+#customer_name = str_i[1].split("---")[2].strip()
+#initial_admin = str_i[2].split("---")[2].strip()
+#print(f"{customer_name}|{initial_admin}")
